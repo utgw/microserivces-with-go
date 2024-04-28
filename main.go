c832e6af4235b2bb21624b0e63ca7ee178e24a42
utgw <utgw.1202@gmail.com>
2024-04-28 15:07:30 +0900

    Add Router

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+    r := mux.NewRouter()
+
+    r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        title := vars["title"]
+        page := vars["page"]
+
+        fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
     })
 
-    http.ListenAndServe(":80", nil)
+    http.ListenAndServe(":80", r)
 }
